a2f8c2e3ea40741cff4fb571e595bbce42471583
Lucas Braga Santos <lucasbrasantos@gmail.com>
2023-08-04 11:42:17 -0300

    update app and delete teste.html

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,5 +1,11 @@
-function App() {
-  return <div>Hello !</div>;
+import React from 'react'
+
+
+
+const App = () => {
+	return (
+		<div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, itaque. Accusantium distinctio tenetur, ipsam repellat amet eligendi. Odit alias dolor fuga quisquam adipisci quas, excepturi laborum? Eius aspernatur iure vitae!</div>
+	)
 }
 
-export default App;
+export default App
