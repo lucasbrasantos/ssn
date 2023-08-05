5d5abf89da2fb81fc87dda7531e4c233a74c66b0
Lucas Braga Santos <lucasbrasantos@gmail.com>
2023-08-05 19:30:21 -0300

    create Login.jsx

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import Login from './pages/Login'
 
 
 const App = () => {
 	return (
-		<div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, itaque. Accusantium distinctio tenetur, ipsam repellat amet eligendi. Odit alias dolor fuga quisquam adipisci quas, excepturi laborum? Eius aspernatur iure vitae!</div>
+		<Login />
 	)
 }
 
