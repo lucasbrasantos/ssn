544e3cc97039ef3866ce766b83ad5aea8d1b5ae0
Lucas Braga Santos <lucasbrasantos@gmail.com>
2023-08-10 09:51:13 -0300

    Update Register.jsx

--- app/src/pages/Register.jsx
+++ app/src/pages/Register.jsx
@@ -1,40 +1,59 @@
 import React from 'react'
-import '../styles/styles.scss'
+import '../styles/main.scss'
 
-const Login = () => {
-    
+
+
+
+const Register = () => {
+
     
     return(
-        <div className="loginContainer">
-
+        <div className="registerContainer">
+            
+            
             <div className="formWrapper">
+                
+                <div id="ball1"></div>
+                <div id="ball2"></div>
+            
+                <img src="../../src/assets/sampleImg.png" alt="test img" />
+
                 <form action="">
-                    <h1>logsin</h1>
+                    <h1>registro</h1>
+
 
                     <div className="inputs">
-                        <input type="text" />
-                        <input type="password" />
+                        <label htmlFor="user">nome</label>
+                        <input id='user' type="text" />
+
+                        <label htmlFor="password">email</label>
+                        <input id='email' type="password" />
+
+                        <label htmlFor="password">senha</label>
+                        <input id='senha' type="password" />
+
+                        <label htmlFor="password">confirmar senha</label>
+                        <input id='re-senha' type="password" />
                     </div>
 
                     <div className="buttons">
-                        <button>entrar</button>
+                        <button>Cadastrar</button>
                         
                         <div className="line">
-                            <hr />
-                            <p>ou</p>
-                            <hr />
+                            <p>Ou</p>
                         </div>
 
-                        <button>login com google</button>
-                        <button>cadastrar-se</button>
+                        <button id='googleBtn'>Login com Google</button>
+                        <button>Login</button>
                     </div>
                 </form>
 
-                <img src="https://www.viewhotels.jp/ryogoku/wp-content/uploads/sites/9/2020/03/test-img.jpg" alt="test img" />
             </div>
+
+            
 
         </div>
     )
 }
 
-export default Login
+export default Register
