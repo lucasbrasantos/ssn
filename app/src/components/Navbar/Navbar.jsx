a4845bb8d2aef8d142cf3d7192ed170e862fc1a0
Lucas Braga Santos <lucasbrasantos@gmail.com>
2023-08-10 08:58:34 -0300

    add styles for each component folder

---
+++ app/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../styles/components.scss'
+
+const Navbar = () => {
+  return (
+    <div id='Navbar'>
+
+        <p>Navbar</p>
+
+    </div>
+  )
+}
+
+export default Navbar
+
+
