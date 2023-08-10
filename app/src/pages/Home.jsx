69c0aacfa13a3b1b725cfe456e83feb80c41f324
Lucas Braga Santos <lucasbrasantos@gmail.com>
2023-08-10 20:34:29 -0300

    Update Home.jsx

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
-import '../styles/HomeStyle.scss';
+import '../styles/main.scss'
+
 
 const Home = () => {
     return(
         <div className='HomeContainer'>
             <nav className='navbar'>
                 <span className='logo'>Logo</span>
-                <span>Icon1</span>
-                <span>Icon2</span>
+
+                <div className="col1">
+                    <span>Icon1</span>
+                    <span>Icon2</span>
+                </div>
 
                 <input className='search' type="text" placeholder='Pesquisar...'/>
 
-                <span>Icon3</span>
-                <span>Icon4</span>
-                <span>Icon5</span>
+                <div className="col2">
+                    <span>Icon3</span>
+                    <span>Icon4</span>
+                    <span>Icon5</span>
+                </div>
 
                 <img className="avatar" src="https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG.png" alt="" />
             </nav>
