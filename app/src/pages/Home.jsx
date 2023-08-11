38dd979450781cb44833299ca6f3ffaa7d4c9a29
ThiagoBaso <thiagorosabaso@gmail.com>
2023-08-11 11:43:22 -0300

    update home

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { cloneElement } from 'react';
 import '../styles/main.scss'
 
 
@@ -23,6 +23,21 @@ const Home = () => {
 
                 <img className="avatar" src="https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG.png" alt="" />
             </nav>
+
+            <div className='postContainer'>
+
+                <div className='postTop'>
+                    <div style={{display:'flex', flexDirection:'row', alignItems:'center', gap:'10px'}}>
+                        <img className="avatar" src="https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG.png" alt="" />
+                        <p>Nome de Ususario</p>
+                    </div>
+                    <button className='followBtn'>Seguir</button>
+                </div>
+
+                <div className='postImg'></div>
+                <div className='postButton'></div>
+            </div>
+            
         </div>
     );
 };
