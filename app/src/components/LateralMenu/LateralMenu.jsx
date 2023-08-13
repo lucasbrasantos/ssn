ef59116a744f3d7be6ec35cbcdcd2f6759e4e0e3
Lucas Braga Santos <lucasbrasantos@gmail.com>
2023-08-13 17:23:08 -0300

    LateralMenu icons

--- app/src/components/LateralMenu/LateralMenu.jsx
+++ app/src/components/LateralMenu/LateralMenu.jsx
@@ -3,12 +3,31 @@ import './style.scss'
 
 const LateralMenu = () => {
   return (
-    <div className='boxMenu'>
-        <div className='opcs'>nome usuario</div>
-        <div className='opcs'>add amigo</div>
-        <div className='opcs'>acessibikidade</div>
-        <div className='opcs'>tema</div>
-        <div className='opcs'>sair</div>
+	<div className='boxMenu'>
+		<div className='opcs opcs-user'>
+			<img src="../../../src/assets/Icon.png" alt="" />
+			nome usuario
+		</div>
+
+		<div className='opcs'>
+			<img src="../../../src/assets/icons/fluent-mdl2_add-friend.png" alt="" />
+			add amigo
+		</div>
+
+		<div className='opcs'>
+			<img src="../../../src/assets/icons/fluent-mdl2_settings.png" alt="" />
+			acessibilidade
+		</div>
+
+		<div className='opcs'>
+			<img src="../../../src/assets/icons/fluent-mdl2_clear-night.png" alt="" />
+			tema
+		</div>
+
+		<div className='opcs'>
+			<img src="../../../src/assets/icons/fluent-mdl2_chrome-back.png" alt="" />
+			sair
+		</div>
 	</div>
   )
 }
