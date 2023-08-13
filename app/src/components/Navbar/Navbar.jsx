41b3a342989c612086ff34cf32ad75dca18223cb
Lucas Braga Santos <lucasbrasantos@gmail.com>
2023-08-13 17:43:45 -0300

    Navbar icons

--- app/src/components/Navbar/Navbar.jsx
+++ app/src/components/Navbar/Navbar.jsx
@@ -7,16 +7,18 @@ const Navbar = () => {
                 <span className='logo'>Logo</span>
 
                 <div className="col1">
-                    <span><img src="../assets/icons/fluent-mdl2_home.png" alt="" />a</span>
-                    <span>Icon2</span>
+                    <span><img src="../../../src/assets/icons/fluent-mdl2_home.png" alt="" /></span>
+                    <span><img src="../../../src/assets/icons/Vector.png" alt="" /></span>
                 </div>
 
-                <input className='search' type="text" placeholder='Pesquisar...'/>
+                <span className='searchSpan'>
+                  <input name='search' className='search' type="text" placeholder='Pesquisar...' />
+                </span>
 
                 <div className="col2">
-                    <span>Icon3</span>
-                    <span>Icon4</span>
-                    <span>Icon5</span>
+                    <span><img src="../../../src/assets/icons/fluent-mdl2_trophy.png" alt="" /></span>
+                    <span><img src="../../../src/assets/icons/fluent-mdl2_office-chat.png" alt="" /></span>
+                    <span><img src="../../../src/assets/icons/fluent-mdl2_message.png" alt="" /></span>
                 </div>
 
                 <img className="avatar" src="https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG.png" alt="" />
