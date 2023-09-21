ed7cfff221edf0d6616e5801f542da634ada8b7d
ThiagoBaso <thiagorosabaso@gmail.com>
2023-09-21 14:41:17 -0300

    mapsAdd

--- app/src/components/Ranking/Ranking.jsx
+++ app/src/components/Ranking/Ranking.jsx
@@ -12,15 +12,53 @@ const Ranking = () => {
 		setSelectedComponent(component)
 	}
 
+	const data = [
+		{
+			"userid": 1,
+			"name": "Stephen Curry",
+			"points": 4245,
+		},
+		{
+			"userid": 2,
+			"name": "Calvo Klein",
+			"points": 3890,
+		},
+		{
+			"userid": 3,
+			"name": "Estêvão Curi",
+			"points": 2350,
+		},
+		{
+			"userid": 4,
+			"name": "Tomas Moreno",
+			"points": 2278,
+		},
+		{
+			"userid": 5,
+			"name": "Herrero Victor",
+			"points": 900,
+		},
+		{
+			"userid": 6,
+			"name": "Andre Pierce",
+			"points": 60,
+		},
+	]
 
 	return (
 		<div id='Ranking'>
 			<h1>Ranking</h1>
 			
 			
-			<RankUser ranking={1} points={2000}/>
-			<RankUser ranking={2} points={1384}/>
-			<RankUser ranking={3} points={1200}/>
+			{
+				data.map(e => {
+					return <RankUser
+					name={e.name}
+					points={e.points}
+					ranking={e.userid}
+					/>
+				})
+			}
 
 		</div>
 	)
