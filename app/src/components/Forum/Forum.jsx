7f037a5edc32049d2b0ee560f3976c60d47bd694
Lucas Braga Santos <lucasbrasantos@gmail.com>
2023-09-25 21:59:05 -0300

    update forum
    axios request from db

--- app/src/components/Forum/Forum.jsx
+++ app/src/components/Forum/Forum.jsx
@@ -1,49 +1,32 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import ForumBlock from '../ForumBlock/ForumBlock.jsx'
 
 import './style.scss'
 
 const Forum = () => {
 
-
-	const data = [
-		
-		{
-			"title": "Opiniões sobre o jogo de ontem?",
-			"username": "Mathias Galvão",
-			"created_at": "2032-09-03T04:42:20.000Z",
-			"description": "Comentários sobre os jogos recentes.",
-			"tag": 'Futebol',
-			"likes": 118,
-		},
-		{
-			"title": "Como arrumo a corrente da minha bicicleta?",
-			"username": "Lucas Baga",
-			"created_at": "2032-09-03T04:42:20.000Z",
-			"description": "Alguem me ajudaaaaaaaaaaaa",
-			"tag": 'bike',
-			"likes": 97,
-		},
-		{
-			"title": "Volei é jogo de pessoas afeminadas?",
-			"username": "Nadiel Voleiss",
-			"created_at": "2032-09-03T04:42:20.000Z",
-			"description": "Me sinto meio estranho apos jogar volei, devo me preocupar ????",
-			"tag": 'Volei',
-			"likes": 2,
-		},
-		{
-			"title": "Neymar ta certo ou ta errado?",
-			"username": "Ribeirinho",
-			"created_at": "2032-09-03T04:42:20.000Z",
-			"description": "date equator arrive directly take club meal was garden finish clearly our available bank fly mysterious national beauty combine brush species health driver whetherlorem",
-			"tag": 'Futebol',
-			"likes": 1638,
-		},
-	]
 	
+	const [data, setData] = useState([])
+	const [interests, setInterests] = useState([])
 
+	useEffect(() => {
+		axios.get('http://localhost:3000/forum')
+		.then(res => setData(res.data))
+		.catch(err => console.log(err))
+
+		axios.get('http://localhost:3000/interests')
+		.then(res => setInterests(res.data))
+		.catch(err => console.log(err))
+
+		
+	}, [])
+
+	function tag(tag){
+
+		const matchingInterest = interests.find((e) => e.interestsid === tag);
+		return matchingInterest.description
+	}
 
 	return (
 		<div id='ForumPage'>
@@ -55,16 +38,20 @@ const Forum = () => {
 
 
 			{
+				(data && data.length > 0) ?
+
 				data.map(e => {
 					return <ForumBlock
 					title={e.title}
 					username={e.username}
 					created_at={e.created_at}
 					description={e.description}
-					tag={e.tag}
+					tag={tag(e.tag)}
 					likes={e.likes}
 					/>
-				})
+				}) : (
+					<p className='noData'>nada aqui</p>
+				)
 			}
 
 
