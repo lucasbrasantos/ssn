fe5783888b68e628352f932a9817eea1adeb47a2
Lucas Braga Santos <lucasbrasantos@gmail.com>
2023-09-26 22:33:20 -0300

    Create AuthContext.jsx

---
+++ app/src/context/AuthContext.jsx
@@ -0,0 +1,31 @@
+import { onAuthStateChanged } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import { auth } from "../firebase.js";
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({children}) => {
+    const [currentUser, setCurrentUser] = useState({})
+
+    useEffect(() => {
+        const unsub = onAuthStateChanged(auth, (user) => {
+            setCurrentUser(user);
+            // console.log(user);
+        });
+
+
+        return () => {
+            unsub(); 
+        };
+
+    }, []);
+
+    return(
+    
+        <AuthContext.Provider value={{currentUser}}>
+            {children}
+        </AuthContext.Provider>
+
+    );
+
+}
