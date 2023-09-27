9b9e9cfc2ed44fe4a639e5b5ab0473b1e105fa62
Lucas Braga Santos <lucasbrasantos@gmail.com>
2023-09-26 22:33:24 -0300

    Update main.jsx

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { AuthContextProvider } from './context/AuthContext.jsx'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-
-		<App />
+	<AuthContextProvider>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	</AuthContextProvider>
 		
-	</React.StrictMode>,
 )
