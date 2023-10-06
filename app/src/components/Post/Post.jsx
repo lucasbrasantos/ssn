50bc7d4436f2f4233954fd07c1e8cc3654117543
ThiagoBaso <thiagorosabaso@gmail.com>
2023-10-06 11:41:21 -0300

    commentsAdded

--- app/src/components/Post/Post.jsx
+++ app/src/components/Post/Post.jsx
@@ -3,14 +3,7 @@ import './style.scss'
 
 const Post = (props) => {
   return (
-	/*
-	<div className="comments">
-		<div className="comment">
-			
-		</div>
-	</div>
-	*/
-	
+	<div>
     <div className='postContainer'>
 
 		<div className='postTop'>
@@ -43,7 +36,100 @@ const Post = (props) => {
 		
 	</div>
 	
-	
+	<div className="comments">
+		<div className="commentBox">
+			<div>
+				<img src="../../../src/assets/Icon.png" alt="" />
+			</div>
+			<div style={{marginRight:'47px'}}>
+				<div style={{display:'flex', flexDirection:'row', gap:'20px', marginBottom:'10px'}}>
+					<p>user name</p>
+					<p>56min</p>
+				</div>
+				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+				<div className='cmtBtn'>
+					<div style={{ display:'flex', flexDirection:'row', gap:'5px'}}>
+						<img src="../../../src/assets/icons/fluent-mdl2_heart.png" alt="" />
+						{props.likes > 0 &&
+							<p className='statusbtn'>{props.likes}</p>
+						}
+					</div>
+					<button>Responder</button>
+				</div>
+			</div>
+		</div>
+
+		<div className="commentBox">
+			<div>
+				<img src="../../../src/assets/Icon.png" alt="" />
+			</div>
+			<div style={{marginRight:'47px'}}>
+				<div style={{display:'flex', flexDirection:'row', gap:'20px', marginBottom:'10px'}}>
+					<p>user name</p>
+					<p>56min</p>
+				</div>
+				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+				<div className='cmtBtn'>
+					<div style={{ display:'flex', flexDirection:'row', gap:'5px'}}>
+						<img src="../../../src/assets/icons/fluent-mdl2_heart.png" alt="" />
+						{props.likes > 0 &&
+							<p className='statusbtn'>{props.likes}</p>
+						}
+					</div>
+					<button>Responder</button>
+				</div>
+			</div>
+		</div>
+		<div className="commentBox">
+			<div>
+				<img src="../../../src/assets/Icon.png" alt="" />
+			</div>
+			<div style={{marginRight:'47px'}}>
+				<div style={{display:'flex', flexDirection:'row', gap:'20px', marginBottom:'10px'}}>
+					<p>user name</p>
+					<p>56min</p>
+				</div>
+				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+				<div className='cmtBtn'>
+					<div style={{ display:'flex', flexDirection:'row', gap:'5px'}}>
+						<img src="../../../src/assets/icons/fluent-mdl2_heart.png" alt="" />
+						{props.likes > 0 &&
+							<p className='statusbtn'>{props.likes}</p>
+						}
+					</div>
+					<button>Responder</button>
+				</div>
+			</div>
+		</div>
+		<div className="commentBox">
+			<div>
+				<img src="../../../src/assets/Icon.png" alt="" />
+			</div>
+			<div style={{marginRight:'47px'}}>
+				<div style={{display:'flex', flexDirection:'row', gap:'20px', marginBottom:'10px'}}>
+					<p>user name</p>
+					<p>56min</p>
+				</div>
+				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+				<div className='cmtBtn'>
+					<div style={{ display:'flex', flexDirection:'row', gap:'5px'}}>
+						<img src="../../../src/assets/icons/fluent-mdl2_heart.png" alt="" />
+						{props.likes > 0 &&
+							<p className='statusbtn'>{props.likes}</p>
+						}
+					</div>
+					<button>Responder</button>
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+	</div>
   )
 }
 
