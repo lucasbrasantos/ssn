abf02e24c698bd019b77803e6a319b41fef5e882
Lucas Braga Santos <lucasbrasantos@gmail.com>
2023-10-12 15:44:43 -0300

    posts update - comments

--- app/src/components/Post/Post.jsx
+++ app/src/components/Post/Post.jsx
@@ -1,136 +1,189 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './style.scss'
+import axios from 'axios'
 
 const Post = (props) => {
-  return (
-	<div>
-    <div className='postContainer'>
-
-		<div className='postTop'>
-			<div style={{display:'flex', flexDirection:'row', alignItems:'center', gap:'10px'}}>
-				<img className="postAvatar" src={props.user_photo_url || "../../../src/assets/Profile-Avatar-PNG.png"} alt="" />
-				<p>{props.username || 'username'}</p>
-			</div>
-			<button className='followBtn'>Seguir</button>
-		</div>
-		
-		<div className='postImg'>
-		<p id='postTitle'>
-				{props.title}
-			</p>
-			<img src={props.photo_url  || "../../../src/assets/sampleImg.png"} alt="" />
-		</div>
-		<div className='postBottom'>
-			<div className='btn1'>
-				<img src="../../../src/assets/icons/fluent-mdl2_heart.png" alt="" />
-				{props.likes > 0 &&
-					<p className='statusbtn'>{props.likes}</p>
-				}
-				<img src="../../../src/assets/icons/fluent-mdl2_message.png" alt="" />
-				<img style={{marginBottom:"6px"}} src="../../../src/assets/icons/fluent-mdl2_share.png" alt="" />
-			</div>
-			<div style={{marginRight:"25px"}}>
-			<img src="../../../src/assets/icons/fluent-mdl2_more-vertical.png" alt="" />
-			</div>
-		</div>
-		
-	</div>
 	
-	<div className="comments">
-		<div className="commentBox">
-			<div>
-				<img src="../../../src/assets/Icon.png" alt="" />
-			</div>
-			<div style={{marginRight:'47px'}}>
-				<div style={{display:'flex', flexDirection:'row', gap:'20px', marginBottom:'10px'}}>
-					<p>user name</p>
-					<p>56min</p>
-				</div>
-				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+	const post = props.postData.post
+	const user = props.postData.user
+	const comment = props.postData.comment
 
-				<div className='cmtBtn'>
-					<div style={{ display:'flex', flexDirection:'row', gap:'5px'}}>
-						<img src="../../../src/assets/icons/fluent-mdl2_heart.png" alt="" />
-						{props.likes > 0 &&
-							<p className='statusbtn'>{props.likes}</p>
-						}
+	// console.log(post);
+	// console.log(user);
+	
+
+
+	const [isCommentsOpen, setIsCommentsOpen] = useState(false)
+	
+	const toggleComment = () => {
+
+		setIsCommentsOpen(!isCommentsOpen);
+		// console.log(isCommentsOpen);
+	}
+
+	function formatDateToYYYYMMDD(dateString) {
+		const date = new Date(dateString);
+		const year = date.getFullYear();
+		const month = (date.getMonth() + 1).toString().padStart(2, '0');
+		const day = date.getDate().toString().padStart(2, '0');
+		const hours = date.getHours().toString().padStart(2, '0');
+  		const minutes = date.getMinutes().toString().padStart(2, '0');
+		return `${year}/${month}/${day} ${hours}:${minutes}`;
+	}
+
+	/*
+	const [_users, set_Users] = useState([])
+	const [_comment, set_Comment] = useState([])
+	const [_posts, set_Posts] = useState([])
+
+	useEffect(() => {
+		fetchData()
+	}, [])
+
+	const fetchData = async() => {
+		await axios.get('http://localhost:3000/posts')
+		.then(res => set_Posts(res.data))
+		.catch(err => console.log(err))
+
+		await axios.get('http://localhost:3000/comment')
+		.then(res => set_Comment(res.data))
+		.catch(err => console.log(err))
+
+		
+		await axios.get('http://localhost:3000/users')
+		.then(res => set_Users(res.data))
+		.catch(err => console.log(err))
+		
+		
+	}
+
+	const dataWithComments  = posts.map((post) => { // e => element = each post in sorted posts
+		const user = users.find(user => user.userid === comment.userid)
+		const comments = _users.find(user => user.userid === comment.userid)
+
+		return{
+			post,
+			comments: comments,
+			user: user,
+		}
+	})
+
+	console.log(commentWithUsers);
+
+*/
+
+	return (
+		<div className='postAll'>
+			<div className='postContainer' id='postContainer'>
+
+				<div className='postTop'>
+					<div style={{display:'flex', flexDirection:'row', alignItems:'center', gap:'10px'}}>
+						<img className="postAvatar" src={user ? user.photourl : "../../../src/assets/Profile-Avatar-PNG.png"} alt="" />
+						<p>{user ? user.username : 'usuario'}</p>
 					</div>
-					<button>Responder</button>
+					<button className='followBtn'>Seguir</button>
 				</div>
-			</div>
-		</div>
-
-		<div className="commentBox">
-			<div>
-				<img src="../../../src/assets/Icon.png" alt="" />
-			</div>
-			<div style={{marginRight:'47px'}}>
-				<div style={{display:'flex', flexDirection:'row', gap:'20px', marginBottom:'10px'}}>
-					<p>user name</p>
-					<p>56min</p>
+				
+				<div className='postImg'>
+				<p id='postTitle'>
+						{post.title}
+					</p>
+					<img src={post.photourl  || "../../../src/assets/sampleImg.png"} alt="" />
 				</div>
-				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-
-				<div className='cmtBtn'>
-					<div style={{ display:'flex', flexDirection:'row', gap:'5px'}}>
+				<div className='postBottom'>
+					<div className='btn1'>
 						<img src="../../../src/assets/icons/fluent-mdl2_heart.png" alt="" />
-						{props.likes > 0 &&
-							<p className='statusbtn'>{props.likes}</p>
+						{post.likes > 0 &&
+							<p className='statusbtn'>{post.likes}</p>
 						}
+						<img src="../../../src/assets/icons/fluent-mdl2_message.png" alt="" id='commentsIcon' onClick={() => toggleComment()} />
+						<img style={{marginBottom:"6px"}} src="../../../src/assets/icons/fluent-mdl2_share.png" alt="" />
 					</div>
-					<button>Responder</button>
-				</div>
-			</div>
-		</div>
-		<div className="commentBox">
-			<div>
-				<img src="../../../src/assets/Icon.png" alt="" />
-			</div>
-			<div style={{marginRight:'47px'}}>
-				<div style={{display:'flex', flexDirection:'row', gap:'20px', marginBottom:'10px'}}>
-					<p>user name</p>
-					<p>56min</p>
-				</div>
-				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-
-				<div className='cmtBtn'>
-					<div style={{ display:'flex', flexDirection:'row', gap:'5px'}}>
-						<img src="../../../src/assets/icons/fluent-mdl2_heart.png" alt="" />
-						{props.likes > 0 &&
-							<p className='statusbtn'>{props.likes}</p>
-						}
+					<div style={{marginRight:"25px"}}>
+					<img src="../../../src/assets/icons/fluent-mdl2_more-vertical.png" alt="" />
 					</div>
-					<button>Responder</button>
 				</div>
+				
 			</div>
-		</div>
-		<div className="commentBox">
-			<div>
-				<img src="../../../src/assets/Icon.png" alt="" />
-			</div>
-			<div style={{marginRight:'47px'}}>
-				<div style={{display:'flex', flexDirection:'row', gap:'20px', marginBottom:'10px'}}>
-					<p>user name</p>
-					<p>56min</p>
-				</div>
-				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+			
+			{
+				isCommentsOpen && (
 
-				<div className='cmtBtn'>
-					<div style={{ display:'flex', flexDirection:'row', gap:'5px'}}>
-						<img src="../../../src/assets/icons/fluent-mdl2_heart.png" alt="" />
-						{props.likes > 0 &&
-							<p className='statusbtn'>{props.likes}</p>
-						}
+					<div className="comments" id='comments'>
+						{
+														
+							comment !== null && (Array.isArray(comment)) ? ( // if comment is not null and is an array
+								comment.map((c, key) => {
+
+									return(
+										<div className="commentBox" key={key}>
+											<div>
+												<img className='commentUserAvatar' src={c ? c.user.photourl : "../../../src/assets/Profile-Avatar-PNG.png"} alt="userphoto" />
+											</div>
+											<div style={{marginRight:'47px'}}>
+												<div style={{display:'flex', flexDirection:'row', gap:'20px', marginBottom:'10px'}}>
+													<p>
+														{c ? c.user.username : 'usuario'}
+													</p>
+													<p>{c ? formatDateToYYYYMMDD(c.timecommented) : 'data'}</p>
+												</div>
+												<p>
+													{c ? c.comment : 'comment'}
+												</p>
+												{/*
+												<div className='cmtBtn'>
+													<div style={{ display:'flex', flexDirection:'row', gap:'5px'}}>
+														<img src="../../../src/assets/icons/fluent-mdl2_heart.png" alt="" />
+														{post.likes > 0 &&
+															<p className='statusbtn'>{post.likes}</p>
+														}
+													</div>
+													<button>Responder</button>
+												</div>
+												*/}
+											</div>
+										</div>
+									)
+								})
+
+							) : comment !== null && !Array.isArray(comment) ? ( // if comment is not null and not array of comments; so its an single comment
+								<div className="commentBox">
+									<div>
+										<img className='commentUserAvatar' src={comment ? comment.user.photourl : "../../../src/assets/Profile-Avatar-PNG.png"} alt="userphoto" />
+									</div>
+									<div style={{marginRight:'47px'}}>
+										<div style={{display:'flex', flexDirection:'row', gap:'20px', marginBottom:'10px'}}>
+											<p>{comment ? comment.user.username : 'usuario'}</p>
+											<p>{comment ? formatDateToYYYYMMDD(comment.timecommented) : 'data'}</p>
+										</div>
+										<p>
+											{comment ? comment.comment : 'comment'}
+										</p>
+
+										<div className='cmtBtn'>
+											<div style={{ display:'flex', flexDirection:'row', gap:'5px'}}>
+												<img src="../../../src/assets/icons/fluent-mdl2_heart.png" alt="" />
+												{post.likes > 0 &&
+													<p className='statusbtn'>{post.likes}</p>
+												}
+											</div>
+											<button>Responder</button>
+										</div>
+									</div>
+								</div>
+							) : (
+								<div className="commentBox">
+									<p>no comments</p>
+								</div>
+							)
+						} 
 					</div>
-					<button>Responder</button>
-				</div>
-			</div>
+
+				)
+			}
+
 		</div>
-
-	</div>
-
-	</div>
-  )
+	)
 }
 
 export default Post
--- app/src/components/Posts/Posts.jsx
+++ app/src/components/Posts/Posts.jsx
@@ -7,6 +7,7 @@ const Posts = () => {
 
 	const [users, setUsers] = useState([])
 	const [posts, setPosts] = useState([])
+	const [comments, setComments] = useState([])
 	const [interests, setInterests] = useState([])
 
 	useEffect(() => {
@@ -22,6 +23,9 @@ const Posts = () => {
 		.then(res => setInterests(res.data))
 		.catch(err => console.log(err))
 
+		await axios.get('http://localhost:3000/comment')
+		.then(res => setComments(res.data))
+		.catch(err => console.log(err))
 		
 		await axios.get('http://localhost:3000/users')
 		.then(res => setUsers(res.data))
@@ -29,22 +33,32 @@ const Posts = () => {
 		
 		
 	}
-
-	
 	
 
 	const sortedPosts = posts.sort((a, b) => new Date(b.timeposted) - new Date(a.timeposted)); // sort posts by timestamp newest to oldest
-	const postWithUsers = sortedPosts.map((post) => { // e => element = each post in sorted posts
+
+	const sortedData = sortedPosts.map((post) => { // e => element = each post in sorted posts
 		const user = users.find(user => user.userid === post.userid)
+		const comment = comments.filter(comment => comment.postid === post.postid)
+
+		let formattedComments = null;
+		if (comment.length === 1) {
+			formattedComments = {...comment[0], user: users.find((user) => user.userid === comment[0].userid) };
+		} else if (comment.length > 1) {
+			// Sort comments by date
+			const sortedComments = comment.sort((a, b) => new Date(b.date) - new Date(a.date));
+			formattedComments = sortedComments.map((comment) => ({ ...comment, user: users.find((user) => user.userid === comment.userid) }));
+		}
 
 		return{
-			...post, user
+			post,
+			user: user,
+			comment: formattedComments,
 		}
-	})
-
+	})	
 	
-	// console.log(postWithUsers);
-	console.log(sortedPosts);
+	// console.log(sortedPosts);
+	// console.log(sortedData);
 	// console.log(users);
 	// console.log(interests);
 
@@ -53,18 +67,14 @@ const Posts = () => {
 			
 			{
 				
-				postWithUsers.length > 0 ? postWithUsers.map((e) => {
+				sortedData.length > 0 ? sortedData.map((data, key) => {
 					
-					
-					
+		
 					return(
 						<Post
-							id={e.id}
-							username={e.user && e.user.username}
-							photo_url={e.photourl}
-							user_photo_url={e.user && e.user.photourl}							
-							likes={e.likes}
-							title={e.title}
+							postData={data}
+
+							key={key}
 						/>
 					)
 				}) : (
