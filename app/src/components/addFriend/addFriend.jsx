5baa7f56a97d0a1d2285bee5bb0cc83a7bdb89b9
ThiagoBaso <thiagorosabaso@gmail.com>
2023-10-19 10:33:51 -0300

    addFriend1.0

--- app/src/components/addFriend/addFriend.jsx
+++ app/src/components/addFriend/addFriend.jsx
@@ -4,26 +4,78 @@ const addFriend = () => {
     return(
         <div className="addFBox">
             <div className="fSearch">
-                <input type="text" />
+                <input type="text" placeholder='Pesquisar...'/>
                 <img src="../../../src/assets/icons/fluent-mdl2_Search.png" alt="" />
             </div>
             <div className="fList">
                 <div className="friendS">
                     <div className='infosF1'>
                         <img className='fAvatar' src="../../../src/assets/Icon.png" alt="" />
-                        <div>
+                        <div style={{display:'flex', flexDirection:'column', gap:'5px'}}>
                             <p>User</p>
                             <p>User Name</p>
                         </div>
                     </div>
                     <div className="infosF2">
-                        <div>
+                        <div style={{display:'flex', flexDirection:'column', gap:'5px', alignItems:'flex-end'}}>
                             <p>N° Followers</p> 
                             <p>Rank User</p>
                         </div>
                         <img src="../../../src/assets/icons/fluent-mdl2_add-friend.png" alt="" />
                     </div>
                 </div>
+
+                
+                <div className="friendS">
+                    <div className='infosF1'>
+                        <img className='fAvatar' src="../../../src/assets/Icon.png" alt="" />
+                        <div style={{display:'flex', flexDirection:'column', gap:'5px'}}>
+                            <p>User</p>
+                            <p>User Name</p>
+                        </div>
+                    </div>
+                    <div className="infosF2">
+                        <div style={{display:'flex', flexDirection:'column', gap:'5px', alignItems:'flex-end'}}>
+                            <p>N° Followers</p> 
+                            <p>Rank User</p>
+                        </div>
+                        <img src="../../../src/assets/icons/fluent-mdl2_add-friend.png" alt="" />
+                    </div>
+                </div>
+                <div className="friendS">
+                    <div className='infosF1'>
+                        <img className='fAvatar' src="../../../src/assets/Icon.png" alt="" />
+                        <div style={{display:'flex', flexDirection:'column', gap:'5px'}}>
+                            <p>User</p>
+                            <p>User Name</p>
+                        </div>
+                    </div>
+                    <div className="infosF2">
+                        <div style={{display:'flex', flexDirection:'column', gap:'5px', alignItems:'flex-end'}}>
+                            <p>N° Followers</p> 
+                            <p>Rank User</p>
+                        </div>
+                        <img src="../../../src/assets/icons/fluent-mdl2_add-friend.png" alt="" />
+                    </div>
+                </div>
+                <div className="friendS">
+                    <div className='infosF1'>
+                        <img className='fAvatar' src="../../../src/assets/Icon.png" alt="" />
+                        <div style={{display:'flex', flexDirection:'column', gap:'5px'}}>
+                            <p>User</p>
+                            <p>User Name</p>
+                        </div>
+                    </div>
+                    <div className="infosF2">
+                        <div style={{display:'flex', flexDirection:'column', gap:'5px', alignItems:'flex-end'}}>
+                            <p>N° Followers</p> 
+                            <p>Rank User</p>
+                        </div>
+                        <img src="../../../src/assets/icons/fluent-mdl2_add-friend.png" alt="" />
+                    </div>
+                </div>
+
+
             </div>
         </div>
     )
