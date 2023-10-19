c305bfd9e71804ce28454a15eefec036c84af583
Lucas Braga Santos <lucasbrasantos@gmail.com>
2023-10-19 10:39:15 -0300

    Update Search.jsx

--- app/src/components/Search/Search.jsx
+++ app/src/components/Search/Search.jsx
@@ -1,68 +1,138 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './style.scss'
 import PostPerfil from '../PostPerfil/PostPerfil'
+import axios from 'axios'
+import { AuthContext } from '../../context/AuthContext'
+import { Box, CircularProgress } from '@mui/material'
 
 const Search = () => {
-  return (
-    <div className='searchBox'>
-        <div className='filtro' style={{width:'100%'}}>
-            <select>
-                <option value="0">Filtro</option>
-                <option value="1">Audi</option>
-                <option value="2">BMW</option>
-                <option value="3">Citroen</option>
-                <option value="4">Ford</option>
-                <option value="5">Honda</option>
-                <option value="6">Jaguar</option>
-                <option value="7">Land Rover</option>
-                <option value="koenigsegg">Koenigsegg</option>
-                <option value="8">Mercedes</option>
-                <option value="9">Mini</option>
-                <option value="10">Nissan</option>
-                <option value="11">Toyota</option>
-                <option value="12">Volvo</option>
-            </select>
-        </div>
-        
-        <div className='searchPosts'>
-            <div className='searchGrid'>
-                <PostPerfil />
-                <PostPerfil />
-                <PostPerfil />
-                <PostPerfil />
-                <PostPerfil />
-                <PostPerfil />
-                <PostPerfil />
-                <PostPerfil />
-                <PostPerfil />
-                <PostPerfil />
-                <PostPerfil />
-                <PostPerfil />
-                <PostPerfil />
-                <PostPerfil />
-                <PostPerfil />
-                <PostPerfil />
-                <PostPerfil />
-                <PostPerfil />
-                <PostPerfil />
-                <PostPerfil />
-                <PostPerfil />
-                <PostPerfil />
-                <PostPerfil />
-                <PostPerfil />
-                <PostPerfil />
-                <PostPerfil />
-                <PostPerfil />
-                <PostPerfil />
-                <PostPerfil />
-                <PostPerfil />
-                <PostPerfil />
-                <PostPerfil />
-            </div>
-       </div>
 
-    </div>
-  )
+	const {currentUser} = useContext(AuthContext);
+	
+	const a = undefined;
+	const [selectedInterest, setSelectedInterests] = useState('all');
+	const [filteredPosts, setFilteredPosts] = useState([]);
+
+	///////////////////
+	
+	const [user, setUser] = useState()
+	const [posts, setPosts] = useState([])
+	const [comments, setComments] = useState([])	
+	const [interests, setInterests] = useState([])
+
+
+	useEffect(() => {
+		fetchData();					
+	}, []);
+
+	const fetchData = async() => {
+		await axios.get('http://localhost:3000/posts')
+		.then(res => setPosts(res.data))
+		.catch(err => console.log(err))
+
+		await axios.get('http://localhost:3000/comment')
+		.then(res => setComments(res.data))
+		.catch(err => console.log(err))
+
+		await axios.get('http://localhost:3000/interests')
+		.then(res => setInterests(res.data))
+		.catch(err => console.log(err))
+
+		await axios.get('http://localhost:3000/user_uid', {
+			params: {
+			  uid: currentUser.uid,
+			},
+		  	})
+		.then((res) => { setUser(res.data[0]) })
+		.catch((err) => { console.error(err); });		
+	}
+
+	///////////////////
+
+	const sortedPosts = user ? posts.sort((a, b) => new Date(b.timeposted) - new Date(a.timeposted)) : null; // sort posts by timestamp newest to oldest
+
+	const data = user ? {
+		user: user,
+		posts: posts.map((post) => {
+			const postComments = comments.filter((c) => c.postid === post.postid)
+			const postInterests = interests.find((i) => i.interestsid === post.tag)
+			return { ...post, comments: postComments, interest: postInterests}
+		}), 
+	} : null
+
+	// console.log(data)
+	
+
+	const handleChange = (selectedValue) => {
+		
+		setSelectedInterests(selectedValue)
+		console.log(selectedValue);
+
+		if(selectedInterest === 'all'){
+			setFilteredPosts(data)
+		}else{
+			setFilteredPosts({users: data.user, posts: data.posts.filter(post => post.interest.interestsid === parseInt(selectedInterest))})
+		}
+		
+
+		// console.log(filteredPosts);
+		// console.log(data);
+		// console.log('id: ' + selectedInterest);
+	}
+
+	return (
+		<div className='searchBox'>
+			<div className='filtro' style={{width:'100%'}}>
+				<select value={selectedInterest} onChange={(e) => {handleChange(e.target.value)}}>
+					
+					<option value='all'>tudo</option>
+					{
+						
+						interests && interests.map((e, i) => (
+
+							<option key={i} value={e.interestsid}>{e.description}</option>
+						))
+					}
+
+				</select>
+			</div>
+			
+			<div className='searchPosts' style={{position:'relative'}}>
+				<div className='searchGrid'>
+					
+				{
+					
+					selectedInterest === 'all' && data ? data.posts.map((e, key) => (
+
+						<PostPerfil
+
+							data={e}
+							key={key}
+						/>
+
+					)) : filteredPosts && data ? filteredPosts.posts.map((e, key) => (
+
+						<PostPerfil
+
+							data={e}
+							key={key}
+						/>
+
+					)) : (
+
+						<Box sx={{ position:'absolute', transform:'translateX(-13px)', display: 'flex', width:'100%', alignItems:'center', justifyContent:'center' }}>
+							<CircularProgress />
+				  		</Box>
+
+					)
+
+				}
+
+				</div>
+			</div>
+
+	 	</div>
+	)
 }
 
 export default Search
