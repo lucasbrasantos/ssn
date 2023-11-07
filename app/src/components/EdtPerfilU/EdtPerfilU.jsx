b1bd7135800594fcee5a569186603f6222a6deac
ThiagoBaso <thiagorosabaso@gmail.com>
2023-11-07 13:53:28 -0300

    EdtPU2

--- app/src/components/EdtPerfilU/EdtPerfilU.jsx
+++ app/src/components/EdtPerfilU/EdtPerfilU.jsx
@@ -8,10 +8,14 @@ const EdtPerfilU = () => {
 
     return(
         <div className="EdtUD">
-            <form action="">
-                <input type="text" placeholder='Name'/>
-                <input type="text" placeholder='User Name'/>
-                <input type="text" placeholder='Bio'/>
+            <form className='FormEdtU' action="">
+                <img className='EdtAvatarUser' src="../../../src/assets/Profile-Avatar-PNG.png" alt="" />
+                <input className='inputEdtU' type="text" placeholder='Name'/>
+                <input className='inputEdtU' type="text" placeholder='User Name'/>
+                <textarea className='inputEdtU' placeholder='Bio' cols="30" rows="10"></textarea>
+                <button className='BtnSaveEdtU'>
+                    Salvar
+                </button>
             </form>
         </div>
     )
